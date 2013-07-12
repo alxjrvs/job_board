bf8f6ad301287bfc7f0ae9a63a0ccf9b596f7d0a
Alex <awheeler@bu.edu>
2013-07-11 20:08:06 -0400

    deleted some non bootstrap stylesheets and updated new.html.erb

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ActiveRecord::Base
-  attr_accessible :category, :description, :how, :location, :needed, :pay, :start, :title, :hours
+  attr_accessible :category, :description, :how, :location, :needed, :pay, :start, :title,:hours
    validates :category, :title, :description, :how, :location, :needed, :how, :pay, presence: true
    #add :start once I figure out date
    # validates :pay, :numericality => { :greater_than => 8 }
