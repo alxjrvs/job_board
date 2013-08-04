a934c0d2589c245aa70c92f2ef2cfe95a521c3d1
Alex <awheeler@bu.edu>
2013-08-04 14:50:04 -0400

    adds dynamic titles, simplifies new views and categories, removes images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+   def after_sign_in_path_for(student)
+    categories_path
+  end
+
+   def after_sign_up_path_for(student)
+    categories_path
+  end
+
+  def after_sign_in_path_for(user)
+    new_job_path
+  end
+  
+   def after_sign_up_path_for(user)
+    new_job_path
+  end
+  
 end
